36c54e16054540fca238eec5cde425a7d2347391
Taras <taras.malchenko@gmail.com>
2022-08-15 16:03:38 +0300

    initial commit

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,4 +1,4 @@
-const langArr = {
+export default {
     "en_1": {
         "ua": "Головна",
         "en": "Main"
@@ -22,6 +22,61 @@ const langArr = {
     "en_6": {
         "ua": "Хочу допомогти",
         "en": "Want to help",
-    }
+    },
+    "en_7": {
+        "ua": "Волонтерьска спільнота",
+        "en": "Volunteer community",
+    },
+    "en_8": {
+        "ua": "Благородно тільки те, що безкорисливо",
+        "en": "Noble is that which is not selfless",
+    },
+    "en_9": {
+        "ua": "Отримати допомогу",
+        "en": "Get help",
+    },
+    "en_10": {
+        "ua": "Ваше ім'я*",
+        "en": "User name*",
+    },
+    "en_11": {
+        "ua": "Відправити ",
+        "en": "Send",
+    },
+    "en_12": {
+        "ua": "Скасувати",
+        "en": "Cancel",
+    },
+    "en_13": {
+        "ua": "Допомога військовим",
+        "en": "Military help",
+    },
+    "en_14": {
+        "ua": "Допомога населенню",
+        "en": "Public help",
+    },
+    "en_15": {
+        "ua": "Документи",
+        "en": "Documents",
+    },
+    "en_16": {
+        "ua": "Звіти",
+        "en": "Reports",
+    },
+    "en_17": {
+        "ua": "Ми активно ведемо наш телеграм канал для того щоб ви могли стежити за роботою нашого фонду",
+        "en": "We actively maintain our Telegram channel  so that you can follow the work of our foundation",
+    },
+    "en_18": {
+        "ua": "Ми у соцмережах:",
+        "en": "Our socials:",
+    },
 }
 
+
+/*
+1-масів із ключей objet keys
+2-forEach по масиву
+3-querySelector по ключу
+4-langArr[item][en_1]
+*/ 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 "use strict"
 
+import langArr from "./lang.js"
 
+/*--------------------------------Burger----------------------------------*/
 const iconHeader = document.querySelector('.header__icon');
 const menuBody = document.querySelector('.menu__body');
 const menuList = document.querySelector('.menu__list');
@@ -14,15 +16,212 @@ if (iconHeader) {
     });
 }
 
+/*--------------------------------Change Language----------------------------------*/
+const menuLang = document.querySelector('.menu__lang');
+const subList = document.querySelector('.menu__sub-list');
+const uaShow = document.querySelector('.uashow')
+const enHidden = document.querySelector('.enhidden');
+const markedEn = document.querySelector('.menu__sub-enlang');
+const markedUa = document.querySelector('.menu__sub-ualang');
+const langHidden = document.querySelector('.menu__langhidden');
+const uaShowSmall = document.querySelector('.uashowsmall')
+const enHiddenSmall = document.querySelector('.enhiddensmall');
 
-const showLang = document.querySelector('.menu__lang');
-const changeLang = document.querySelector('.menu__sub-list');
-
-
-if (showLang) {
-    showLang.addEventListener("click", (e) => {
-        changeLang.classList.toggle('_open');
+if (menuLang) {
+    menuLang.addEventListener("click", (e) => {
+        subList.classList.toggle('_open');
+        checkOpenList();
     });
 }
+
+if (langHidden) {
+    langHidden.addEventListener("click", (e) => {
+        subList.classList.toggle('_open');
+        checkOpenList();
+    });
+}
+
+
+
+function checkOpenList() {
+    if (subList.classList.contains('_open')) {
+        subList.addEventListener("click", langListener)
+    } else {
+        subList.removeEventListener("click", langListener)
+    }
+}
+
+const langListener = (e) => {
+    const lang = e.target.closest('LI').getAttribute('data-lang');
+    subList.classList.remove('_open');
+    enHidden.classList.toggle('_open');
+    uaShow.classList.toggle('_close');
+    enHiddenSmall.classList.toggle('_open');
+    uaShowSmall.classList.toggle('_close');
+    changeLang(lang);
+    markLangEng();
+
+};
+
+
+function markLangEng() {
+    if (enHidden.classList.contains('_open')) {
+        markedEn.classList.add('_chosen');
+        markedUa.classList.add('_notchosen');
+    } else {
+        markedEn.classList.remove('_chosen');
+        markedUa.classList.remove('_notchosen');
+    }
+}
+
+
+
+function changeLang(lang) {
+    let chooseDefi = Object.keys(langArr);
+    chooseDefi.forEach(langKey => {
+        let elemForTrans = document.querySelectorAll(`[lang-key="${langKey}"]`);
+        for (let element of elemForTrans) {
+            element.textContent = langArr[langKey][lang];
+        }
+    });
+}
+let username = document.getElementById("username");
+let useremail = document.getElementById("useremail");
+let usertel = document.getElementById("usertel");
+let userprob = document.getElementById("userprob");
+let enBtn = document.getElementById("enbtn");
+let ukrBtn = document.getElementById("ukrbtn");
+
+enBtn.addEventListener('click', () => {
+    username.placeholder = 'User name*';
+    useremail.placeholder = 'User email*';
+    usertel.placeholder = 'User telephone*';
+    userprob.placeholder = 'Describe your problem*';
+})
+
+ukrBtn.addEventListener('click', () => {
+    username.placeholder = 'Ваше ім\'я*';
+    useremail.placeholder = 'Ваш e-mail*';
+    usertel.placeholder = 'Ваш телефон*';
+    userprob.placeholder = 'Опишіть Вашу проблему*';
+})
+
+
+/*-------------------------Popup-------------------------------------*/
+
+const popupBtn = document.querySelector('.helpblock__btnpopup');
+const popup = document.querySelector('.popup');
+const popupContent = document.querySelector('.popup__content');
+const body = document.querySelector('body');
+const popupClose = document.querySelector('.cancel');
+let scroll = calcScroll();
+
+popupBtn.addEventListener("click", (e) => {
+    e.preventDefault
+    popup.classList.add('_open');
+    popupContent.classList.add('_open');
+    body.classList.add('_lock');
+    document.body.style.marginRight = `${scroll}px`;
+});
+
+popupClose.addEventListener("click", (e) => {
+    e.preventDefault
+    popup.classList.remove('_open');
+    popupContent.classList.remove('_open');
+    body.classList.remove('_lock');
+    document.body.style.marginRight = `0px`;
+});
+
+function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.which == 27) {
+        popup.classList.remove('_open');
+        popupContent.classList.remove('_open');
+        body.classList.remove('_lock');
+        slider.classList.remove('_open');
+        pictureFull.remove();
+        document.body.style.marginRight = `0px`;
+    }
+});
+
+/*------------------------Gallery--------------------------------------*/
+
+
+const cards = Array.from(document.querySelectorAll('.documentblock__card'));
+const picture = Array.from(document.querySelectorAll('.documentblock__img'));
+const slider = document.querySelector('.slider');
+const sliderBody = document.querySelector('.slider__body');
+const sliderBtnLeft = document.querySelector('.slider__left');
+const sliderBtnRight = document.querySelector('.slider__right');
+const sliderClose = document.querySelector('.slider__close');
+
+let cardIndex = -1;
+let pictureFull;
+
+for (const card of cards) {
+    card.addEventListener('click', (event) => {
+        event.preventDefault();
+        cardIndex = cards.indexOf(card);
+        pictureFull = picture[cardIndex].cloneNode();
+        sliderBody.append(pictureFull);
+        slider.classList.add('_open');
+        body.classList.add('_lock');
+        document.body.style.marginRight = `${scroll}px`;
+    });
+}
+
+
+sliderBtnLeft.addEventListener("click", (event) => {
+    event.preventDefault();
+    changePicture("left");
+})
+
+
+sliderBtnRight.addEventListener("click", (event) => {
+    event.preventDefault();
+    changePicture("right");
+})
+
+function changePicture(dir) {
+    if (dir === "left") {
+        if (cardIndex > 0) {
+            cardIndex--;
+        } else {
+            cardIndex = cards.length - 1;
+        }
+    } else if (dir === "right") {
+        if (cardIndex < cards.length - 1) {
+            cardIndex++;
+        } else {
+            cardIndex = 0;
+        }
+    }
+    let newPictureFull = picture[cardIndex].cloneNode();
+    pictureFull.replaceWith(newPictureFull);
+    pictureFull = newPictureFull;
+
+}
+
+sliderClose.addEventListener("click", (event) => {
+    event.preventDefault();
+    slider.classList.remove('_open');
+    pictureFull.remove();
+    body.classList.remove("_lock");
+    document.body.style.marginRight = `0px`;
+});
 
 
