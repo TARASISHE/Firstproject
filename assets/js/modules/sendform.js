4ef115279861835ff68d216251a0f5e29d62b60e
Taras <taras.malchenko@gmail.com>
2022-08-24 14:40:23 +0300

    initial commit

--- assets/js/modules/sendform.js
+++ assets/js/modules/sendform.js
@@ -10,20 +10,64 @@ document.addEventListener("DOMContentLoaded", function () {
         let formData = new FormData(form);
 
         if (error === 0) {
-            form.classList.add('_sending');
-            let response = await fetch('sendmail.php', {
+            //form.classList.add('_sending');
+            let response = await fetch('https://jsonplaceholder.typicode.com/posts', {
                 method: 'POST',
                 body: formData
             });
             if (response.ok) {
-                let result = await response.json();
-                alert(result.message);
-                formPreview.innerHtml = '';
+                let json = await response.json();
                 form.reset();
-                form.classList.remove('_sending');
+            } else {
+                alert("Ошибка HTTP: " + response.status);
             }
+        } else {
+            alert("Enter obligatory fields")
         }
     }
+
+    try {
+        function formValidate(form) {
+            let error = 0;
+            let formReq = document.querySelectorAll('._req');
+
+            for (let index = 0; index < formReq.length; index++) {
+                const input = formReq[index];
+                formRemoveError(input);
+
+                if (input.classList.contains('_email')) {
+                    if (emailTest(input)) {
+                        formAddError(input);
+                        error++;
+                    }
+                } else {
+                    if (input.value === '') {
+                        formAddError(input);
+                        error++
+                    }
+                }
+            }
+            return error;
+        }
+
+        function formAddError(input) {
+            input.parentElement.classList.add('_error');
+            input.classList.add('_error');
+        }
+
+        function formRemoveError(input) {
+            input.parentElement.classList.remove('_error');
+            input.classList.remove('_error');
+        }
+
+        function emailTest(input) {
+            return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+        }
+
+    } catch (error) {
+        alert('error');
+    };
+
 
     function formValidate(form) {
         let error = 0;
