402bf2a86ba931e51823ae4d90f303a06767e2b0
Taras <taras.malchenko@gmail.com>
2022-08-24 14:49:15 +0300

    initial commit

--- assets/js/modules/sendform.js
+++ assets/js/modules/sendform.js
@@ -1,72 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
-
-    async function formSend(e) {
-        e.preventDefault();
-
-        let error = formValidate(form);
-
-        let formData = new FormData(form);
-
-        if (error === 0) {
-            //form.classList.add('_sending');
-            let response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-                method: 'POST',
-                body: formData
-            });
-            if (response.ok) {
-                let json = await response.json();
-                form.reset();
-            } else {
-                alert("Ошибка HTTP: " + response.status);
-            }
-        } else {
-            alert("Enter obligatory fields")
-        }
-    }
 
     try {
-        function formValidate(form) {
-            let error = 0;
-            let formReq = document.querySelectorAll('._req');
+        form.addEventListener('submit', formSend);
+        async function formSend(e) {
+            e.preventDefault();
 
-            for (let index = 0; index < formReq.length; index++) {
-                const input = formReq[index];
-                formRemoveError(input);
+            let error = formValidate(form);
 
-                if (input.classList.contains('_email')) {
-                    if (emailTest(input)) {
-                        formAddError(input);
-                        error++;
-                    }
-                } else {
-                    if (input.value === '') {
-                        formAddError(input);
-                        error++
-                    }
+            let formData = new FormData(form);
+
+            if (error === 0) {
+                //form.classList.add('_sending');
+                let response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                    method: 'POST',
+                    body: formData
+                });
+                if (response.ok) {
+                    let json = await response.json();
+                    form.reset();
                 }
+            } else {
+                alert("Enter obligatory fields")
             }
-            return error;
         }
-
-        function formAddError(input) {
-            input.parentElement.classList.add('_error');
-            input.classList.add('_error');
-        }
-
-        function formRemoveError(input) {
-            input.parentElement.classList.remove('_error');
-            input.classList.remove('_error');
-        }
-
-        function emailTest(input) {
-            return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-        }
-
-    } catch (error) {
-        alert('error');
-    };
+    } catch {
+        alert("Error");
+    }
 
 
     function formValidate(form) {
