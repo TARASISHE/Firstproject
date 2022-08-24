c18735d0602efc2ec8a648de3030624597e87531
Taras <taras.malchenko@gmail.com>
2022-08-24 14:50:44 +0300

    initial commit

--- assets/js/modules/sendform.js
+++ assets/js/modules/sendform.js
@@ -20,8 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     let json = await response.json();
                     form.reset();
                 }
-            } else {
-                alert("Enter obligatory fields")
             }
         }
     } catch {
